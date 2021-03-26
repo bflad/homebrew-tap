c29c46037c9a71e7881b2a44543b66b797d4b610
goreleaserbot <goreleaser@carlosbecker.com>
2021-03-26 13:22:23 -0400

    Brew formula update for tfproviderlint version v0.26.0

--- Formula/tfproviderlint.rb
+++ Formula/tfproviderlint.rb
@@ -5,16 +5,16 @@
 class Tfproviderlint < Formula
   desc "Terraform Provider Linter"
   homepage "https://github.com/bflad/tfproviderlint"
-  version "0.25.0"
+  version "0.26.0"
   bottle :unneeded
 
   if OS.mac? && Hardware::CPU.intel?
-    url "https://github.com/bflad/tfproviderlint/releases/download/v0.25.0/tfproviderlint_0.25.0_darwin_amd64.tar.gz"
-    sha256 "365a517cff0929ade4d9150d5f68171e42f246a99750795483a630583e20566e"
+    url "https://github.com/bflad/tfproviderlint/releases/download/v0.26.0/tfproviderlint_0.26.0_darwin_amd64.tar.gz"
+    sha256 "d6b305deb5715c001e89b9204b5084f43a8dafa5317c2e862149355476d55922"
   end
   if OS.linux? && Hardware::CPU.intel?
-    url "https://github.com/bflad/tfproviderlint/releases/download/v0.25.0/tfproviderlint_0.25.0_linux_amd64.tar.gz"
-    sha256 "354d4967dc1361236f860b3c5b63959ff91e4ddea251a09568060d33ed51601b"
+    url "https://github.com/bflad/tfproviderlint/releases/download/v0.26.0/tfproviderlint_0.26.0_linux_amd64.tar.gz"
+    sha256 "d56b8a970c626f30b21a46315bae523c024650e64c416bc3d22120b9af3b8cf3"
   end
 
   depends_on "go"
